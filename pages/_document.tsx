d6f0aa13e4c7d8c96a5a63f27e5469c12cd6e223
Justin Elmore <jelmore1674@pm.me>
2022-06-14 20:35:49 -0500

    add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
 	return (
@@ -13,6 +14,21 @@ export default function Document() {
 				<link
 					href='https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap'
 					rel='stylesheet'
+				/>
+				{/* Google Analytics */}
+				<Script
+					async
+					src='https://www.googletagmanager.com/gtag/js?id=G-FVVD7T5LYY'
+				/>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+					 		window.dataLayer = window.dataLayer || [];
+  							function gtag(){dataLayer.push(arguments);}
+  							gtag('js', new Date());
+  							gtag('config', 'G-FVVD7T5LYY');
+				`,
+					}}
 				/>
 			</Head>
 			<body>
