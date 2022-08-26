600923daeb0f94094a6b33432f037af6cfe3f004
Justin Elmore <jelmore1674@pm.me>
2022-08-25 23:52:46 -0500

    move to monorepo

--- apps/web/lib/apollo.ts
+++ apps/web/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-	uri: process.env.GRAPHQL_API,
+	uri: process.env.NEXT_PUBLIC_GRAPHQL_API,
 	cache: new InMemoryCache(),
 });
