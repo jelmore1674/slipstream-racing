1d73edc1a96476344f8cf2179c317f51b477faa1
Justin Elmore <jelmore1674@pm.me>
2022-08-26 01:23:44 -0500

    got both cms and web working together

--- apps/cms/keystone.ts
+++ apps/cms/keystone.ts
@@ -15,19 +15,22 @@ import { lists } from './schema';
 import { withAuth, session } from './auth';
 
 export default withAuth(
-  // Using the config function helps typescript guide you to the available options.
-  config({
-    // the db sets the database provider - we're using sqlite for the fastest startup experience
-    db: {
-      provider: 'sqlite',
-      url: 'file:./keystone.db',
-    },
-    // This config allows us to set up features of the Admin UI https://keystonejs.com/docs/apis/config#ui
-    ui: {
-      // For our starter, we check that someone has session data before letting them see the Admin UI.
-      isAccessAllowed: (context) => !!context.session?.data,
-    },
-    lists,
-    session,
-  })
+	// Using the config function helps typescript guide you to the available options.
+	config({
+		// the db sets the database provider - we're using sqlite for the fastest startup experience
+		db: {
+			provider: 'sqlite',
+			url: 'file:./keystone.db',
+		},
+		server: {
+			port: 3005,
+		},
+		// This config allows us to set up features of the Admin UI https://keystonejs.com/docs/apis/config#ui
+		ui: {
+			// For our starter, we check that someone has session data before letting them see the Admin UI.
+			isAccessAllowed: (context) => !!context.session?.data,
+		},
+		lists,
+		session,
+	})
 );
